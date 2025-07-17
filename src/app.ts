572e7d902d928a85f0e3760e842904fd9fecfeb4
michael <michaelblossom8654@gmail.com>
2025-07-17 11:44:06 +0100

    first commit

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { response } from "express";
+import cors from "cors";
+// import cookieParser from "cookie-parser";
+
+import morgan from "morgan";
+
+const app = express();
+
+// Global MIDDLEWARES'
+// global middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// body parser
+app.use(express.json({ limit: "10kb" }));
+
+// app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+
+app.use(express.static(`${__dirname}/public`)); //for serving static files
+
+export default app;
