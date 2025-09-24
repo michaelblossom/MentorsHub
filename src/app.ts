19f412c67a3427b4222ac61b82ac29b199e1a144
michael <michaelblossom8654@gmail.com>
2025-09-24 18:29:44 +0100

    Implemented rate limiter that will help control how many request that can be sent from one IP Address per hour

--- src/app.ts
+++ src/app.ts
@@ -12,14 +12,24 @@ import reviewRouter from "./routes/review.route";
 import cookieParser from "cookie-parser";
 
 import morgan from "morgan";
+const rateLimit = require("express-rate-limit"); //npm package that prevents multiple request from one Ip-address
 
 const app = express();
 
 // Global MIDDLEWARES'
-// global middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// ratelimiter for regulating request from same ip
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this Ip, please try again in an hour time",
+});
+
+// making the limitter to work in all api starting with  /api (Limiter is a middle so we can apply app.use on it)
+app.use("/api", limiter);
 
 // body parser
 app.use(express.json({ limit: "10kb" }));
