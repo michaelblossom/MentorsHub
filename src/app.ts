c616e9de5cde825e6bb33d41f5185b75f8d20ca8
michael <michaelblossom8654@gmail.com>
2025-09-29 20:32:21 +0100

    increase rate limiter to 200 per hour

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "development") {
 
 // ratelimiter for regulating request from same ip
 const limiter = rateLimit({
-  max: 100,
+  max: 200,
   windowMs: 60 * 60 * 1000,
   message: "Too many request from this Ip, please try again in an hour time",
 });
