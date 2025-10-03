bc142c9dc1a9df02b929b1b6be2b04a716867ece
michael <michaelblossom8654@gmail.com>
2025-10-03 14:44:48 +0100

    integrate helmet for handlling HTTP hedaers

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -7,7 +7,6 @@ const router = express.Router();
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
-router.post("/login", authController.login);
 router.post("/logout", authController.logout);
 router.post("/forgot-password", authController.forgotPassword);
 router.post("/reset-password", authController.resetPassword);
